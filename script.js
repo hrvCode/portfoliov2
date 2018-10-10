4988cdea9d2749435bab137389ab175954e83b88
jonathans <jonathan.silkewall@gmail.com>
2018-10-10 21:59:21 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+
+// COPIED CODE FROM
+// https://stackoverflow.com/questions/7717527/smooth-scrolling-when-clicking-an-anchor-link
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        console.log(e);
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth',
+            block: "start"
+        });
+    });
+});
